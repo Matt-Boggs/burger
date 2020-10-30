1ef1d894e6c492451c999d57993062044cb6d14f
Matt Boggs <matt.david.boggs@gmail.com>
2020-10-30 17:37:40 -0600

    basic config set up

---
+++ config/orm.js
@@ -0,0 +1,17 @@
+let connection = require("./connection.js")
+
+
+let orm = {
+    selectall: function(){
+
+    },
+
+    insertOne: function(){
+
+    },
+
+    updateOne: function(){
+
+    }
+}
+module.exports = orm
