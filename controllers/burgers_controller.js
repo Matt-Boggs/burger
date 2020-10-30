032fce7f83d62d815fbd9e227e3a71c186bdb682
Matt Boggs <matt.david.boggs@gmail.com>
2020-10-30 17:54:56 -0600

    more directories

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,4 @@
+let express = require("express");
+let burger = require("../models/burger.js")
+
+// Create router for app, and export router at the end of file
---
+++ models/burger.js
@@ -0,0 +1,5 @@
+let orm = require("../config/orm.js")
+
+// Call the orm functions using burger specific input for the orm
+
+module.exports 
