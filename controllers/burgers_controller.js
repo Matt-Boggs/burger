ccac188b00cd8d020ebc6b38a2bbde0c2421a228
Matt Boggs <matt.david.boggs@gmail.com>
2020-10-31 14:11:00 -0600

    framework for routing

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,5 +4,20 @@ let burger = require("../models/burger.js")
 // Create router for app, and export router at the end of file
 let router = express.router()
 
+router.get("/",(req,res)=>{
+
+
+    res.render("index",object)
+});
+
+router.post("",(req,res)=>{
+
+
+    res.json({key: value})
+});
+
+router.put("",(req,res)=>{
+
+})
 
 module.exports = router
