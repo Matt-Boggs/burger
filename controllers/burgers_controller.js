2cd872a73314f6ef1a67df5cecdd89cd7ea4b493
Matt Boggs <matt.david.boggs@gmail.com>
2020-10-31 13:48:31 -0600

    beginning of routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,7 +2,7 @@ let express = require("express");
 let burger = require("../models/burger.js")
 
 // Create router for app, and export router at the end of file
-let router 
+let router = express.router()
 
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 let orm = require("./config/orm.js");
 let express = require("express")
+let app = express();
+let PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
+
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
