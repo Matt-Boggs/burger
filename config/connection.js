6839ce0c3f9ee2d6e63667bf171a961d16972a03
Matt Boggs <matt.david.boggs@gmail.com>
2020-11-03 17:54:52 -0700

    hm

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,15 @@
 var mysql = require("mysql");
-var personal = require("./personal")
 var connection;
 if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  var connection = personal
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Funhaus3121!",
+    database: "burgers_db"
+  });
 }
 
 connection.connect(function(err) {
